a68df54f63dc622a668663ccb61e3f00b9bf755c
Mox Biggs <Mox@Moxs-MacBook-Pro-2.local>
2014-11-05 16:56:51 -0800

    first commit

---
+++ todo_app/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create(title: "Wash dishes")
+Todo.create(title: "Shut the fuck up", notes: "Scheduled time is 9am.")
+Todo.create(title: "Call bank about transfer", notes: "It's been  3 days and transfer hasn't been done.")
+Todo.create(title: "Watch football", notes: "If we lose again, the manager might get sacked!")
+Todo.create(title: "Register for new Tealeaf course")
